af3778425d1875045a5988130377d0f919c8aeb8
Dani <dustyglass@gmail.com>
2024-08-19 21:19:25 -0400

    update instagram link

--- pages/fotografia.tsx
+++ pages/fotografia.tsx
@@ -17,7 +17,7 @@ return (
   <Layout>
     <h2>Fotografía</h2> 
     <div>
-    <p>Revisa todas las fotos de Dani en <Link href="https://www.instagram.com/howl.pictures/">Instagram</Link></p>
+    <p>Revisa todas las fotos de Dani en <Link href="https://www.instagram.com/dani.cortes.dc/">Instagram</Link></p>
     <p onClick={()=>{
               {setHidden(!hidden)}
               {setShowModal(!showModal)} }} 
